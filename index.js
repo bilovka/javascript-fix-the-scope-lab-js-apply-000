763a1e2910b9d4d86eba5f7d9d43ec4a0d22d1cc
Oleksandr <bilovka10@gmail.com>
2020-08-05 20:19:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,9 +12,8 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
  var myanimal = 'cat'
- if(animal == myanimal) 
  return myanimal
- else return animal
+ return animal
 }
 
 function add2(n) {
